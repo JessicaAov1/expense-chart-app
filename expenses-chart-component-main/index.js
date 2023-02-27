e3bce598ee9f992ee0b973e61455fec1b768a99c
JessicaAov1 <86833978+JessicaAov1@users.noreply.github.com>
2023-02-27 18:05:09 +0000

    changed labels in the chart

--- expenses-chart-component-main/index.js
+++ expenses-chart-component-main/index.js
@@ -18,7 +18,7 @@ async function getData() {
       labels: labels,
       datasets: [
         {
-          label: "day",
+          label: "$",
           backgroundColor: [
             "hsl(10, 79%, 65%)",
             "hsl(10, 79%, 65%)",
@@ -35,28 +35,14 @@ async function getData() {
     },
     options: {
       legend: { display: false },
-      title: {
-        display: true,
-        text: "Spending- Last 7 days",
+      scales: {
+        x: {
+          type: "time",
+          time: {
+            unit: "day",
+          },
+        },
       },
     },
   });
-  document
-    .getElementById("myCanvas")
-    .setAttribute("style", "display:block;width:300px;");
-  document.getElementById("myCanvas").style.width = "300px";
-  const canvas = document.getElementById("myCanvas");
-  canvas.addEventListener(
-    "mouseenter",
-    (event) => {
-      // highlight the mouseenter target
-      event.target.style.color = "purple";
-
-      // reset the color after a short delay
-      setTimeout(() => {
-        event.target.style.color = "";
-      }, 500);
-    },
-    false
-  );
 }
